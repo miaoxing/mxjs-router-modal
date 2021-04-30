65e13156f898016d3fe89dbd4348c96351a1bc76
twinh <twinhuang@qq.com>
2021-04-30 15:02:11 +0800

    test(router-modal): 初始化测试

---
+++ __tests__/ModelView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import ModalView from '../ModalView';
+import {MemoryRouter} from 'react-router';
+
+describe('ModalView', () => {
+  test('basic', () => {
+    const result = render(<MemoryRouter>
+      <ModalView/>
+    </MemoryRouter>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
+
